a2649f00b4159b5717a70622d355316107fe014c
FuerzaDON <luis_fernando_chavez@hotmail.com>
2020-02-13 21:56:27 -0600

    consumer file created

--- src/main/java/com/github/simplesteph/kafka/tutorial1/ConsumerDemo.java
+++ src/main/java/com/github/simplesteph/kafka/tutorial1/ConsumerDemo.java
@@ -1,19 +1,58 @@
 package com.github.simplesteph.kafka.tutorial1;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
 public class ConsumerDemo {
-   Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
 
-   String bootstrapServers = "127.0.0.1";
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
 
-   Properties properties = new Properties();
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "my-fourth-application";
+        String topic = "test_topic";
 
-    public void setProperties(Properties properties) {
-        this.properties = properties;
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+
+        //we have 3 differents values: earliest/lastest/none
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        //create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        //Subcribe consumer to our topic(s)
+        //consumer.subscribe(Collections.singleton("first_topic"));
+        //consumer.subscribe(Array.asList("first_topic", "second_topic"));
+        consumer.subscribe(Arrays.asList(topic));
+
+        //poll for new data
+        while(true){
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(100));  // new in kafka 2.0.0
+
+            for(ConsumerRecord<String, String> record: records){
+                logger.info("key: " + record.key() + ", Value "+ record.value());
+                logger.info("Partition: " + record.partition() + ", Offset:" + record.offset());
+            }
+        }
+
+
     }
+
+
+
 }
